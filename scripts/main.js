f70ba7378ddb75113a05160856f58001089b43b0
Bobby Lin <bobbylinjh@gmail.com>
2015-12-06 19:44:50 +0800

    Added generateQuote() in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,23 @@
  *     Author: Bobby Lin
  */
 
+function generateQuote() {
+    currentIndex = generateNextRandomInt();
+    console.log(currentIndex);
+    document.getElementById('text').innerHTML = data[currentIndex].text;
+    document.getElementById('origin').innerHTML = data[currentIndex].origin;
+}
+
+function generateRandomIntFromInterval() {
+    return Math.floor(Math.random()*(data.length));
+}
+
+function generateNextRandomInt() {
+    var num = generateRandomIntFromInterval();
+    while(num == currentIndex) {
+        num = generateRandomIntFromInterval();
+    }
+    return num;
+}
+
+var currentIndex = generateRandomIntFromInterval();
