f355d91d39b19f3245291d5b68ad327777f3562f
Bobby Lin <bobbylinjh@gmail.com>
2015-12-06 22:14:00 +0800

    Added  twitter tweet button in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,7 @@ function generateQuote() {
     console.log(currentIndex);
     document.getElementById('text').innerHTML = data[currentIndex].text;
     document.getElementById('origin').innerHTML = data[currentIndex].origin;
+    createButton();
 }
 
 function generateRandomIntFromInterval() {
@@ -23,3 +24,39 @@ function generateNextRandomInt() {
 }
 
 var currentIndex = generateRandomIntFromInterval();
+
+function createButton() {
+    var msg = document.getElementById("text").innerHTML;
+    msg = modifyStringToWordLimit(msg);
+    var tweetDiv = document.querySelector(".twitter-share-button");
+    var link = document.createElement("a");
+
+    link.setAttribute("href", "https://twitter.com/share");
+    link.setAttribute("class", "twitter-share-button");
+    link.setAttribute('id', 'twitter');
+    link.setAttribute("data-text", "" + msg + "");
+    link.setAttribute("data-size", "large");
+    tweetDiv.parentNode.replaceChild(link, tweetDiv);
+    twttr.widgets.load();
+}
+
+function modifyStringToWordLimit(msg) {
+    if(msg.length >=80) {
+        msg = msg.substring(0,75);
+        msg += '..."';
+    }
+    return msg;
+}
+
+// Twitter API for posting single tweet
+!function(d, s, id){
+    var js,
+        fjs=d.getElementsByTagName(s)[0],
+        p=/^http:/.test(d.location)?'http':'https';
+    if(!d.getElementById(id)) {
+        js=d.createElement(s);
+        js.id=id;
+        js.src=p+'://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+}(document, 'script', 'twitter-wjs');
