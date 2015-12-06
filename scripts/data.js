b8f9f04331f14916406d4099203dc5ca28614864
Bobby Lin <bobbylinjh@gmail.com>
2015-12-06 19:35:39 +0800

    Added data.js to store quotes in JSON objects

---
+++ scripts/data.js
@@ -0,0 +1,17 @@
+/**
+ * Created on: 6/12/15
+ *     Author: Bobby Lin
+ */
+
+var data = [
+    {"text" :"“Everyone is the other and no one is himself.”",
+        "origin":"Martin Heidegger"},
+    {"text" :"“Man acts as though he were the shaper and master of language, " +
+    "while in fact language remains the master of man.”",
+        "origin":"Martin Heidegger"},
+    {"text" :"“The body is our general medium for having a world.”",
+        "origin":" Maurice Merleau-Ponty"},
+    {"text" :"“We must therefore rediscover, after the natural world, the social world, " +
+    "not as an object or sum of objects, but as a permanent field or dimension of existence.” ",
+        "origin":" Maurice Merleau-Ponty"}
+];
